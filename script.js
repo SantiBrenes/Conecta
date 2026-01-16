fe15e36bbc90e487db83f3ed4aca58a92d3ff0d0
SantiBrenes <110999358+SantiBrenes@users.noreply.github.com>
2026-01-16 10:40:37 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function mostrar(id) {
   const pages = document.querySelectorAll('.page');
-  pages.forEach(p => p.classList.remove('show'));
 
-  document.getElementById(id).classList.add('show');
+  pages.forEach(p => {
+    p.classList.remove('show');
+  });
 
+  const target = document.getElementById(id);
+  if (target) {
+    target.classList.add('show');
+  }
+
+  // Cerrar menú hamburguesa si está abierto
   const menu = document.querySelector('.menu');
   menu.classList.remove('show');
 }
